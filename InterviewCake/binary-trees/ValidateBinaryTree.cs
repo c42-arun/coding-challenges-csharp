08ff321060918b9e5fc52bcd955bec532d351bf6
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-03-26 08:45:36 +0000

    TODO: IsBinarySearchTree

---
+++ InterviewCake/binary-trees/ValidateBinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCake.binary_trees
+{
+    public class ValidateBinaryTree
+    {
+        public static bool IsBinarySearchTree(BinaryTreeNode root)
+        {
+            return CheckNode(root, double.NegativeInfinity, double.NegativeInfinity);
+        }
+
+        //private static bool CheckNode(BinaryTreeNode node, double lowerBound, double upperBound)
+        //{
+        //    if (node == null) // if root node is null this is not a valid binary tree
+        //        return false;
+
+        //    if (node.Value < lowerBound || node.Value > upperBound)
+        //        return false;
+
+        //    if (node.Left != null)
+        //    { 
+        //        bool leftNodeCheck = CheckNode(node.Left, lowerBound, node.Value);
+        //        if (!leftNodeCheck) return false;
+        //}
+    }
+}
