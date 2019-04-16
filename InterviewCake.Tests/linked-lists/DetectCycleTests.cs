03c68fe527979dc5c7def3a9db869d954e9bf8a7
Arun Ramachandriya <arun.ramachandriya@investec.co.uk>
2019-04-16 11:32:45 +0100

    Singly LL - detect cycle

---
+++ InterviewCake.Tests/linked-lists/DetectCycleTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterviewCake.linked_lists;
+using InterviewCake.linked_lists.singly_linked;
+using Xunit;
+
+namespace InterviewCake.Tests.linked_lists
+{
+    public class DetectCycleTests
+    {
+        [Fact]
+        public void LinkedListWithNoCycleBruteForceTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(nodes[0]);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CycleLoopsToBeginningBruteForceTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            nodes[3].Next = nodes[0];
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(nodes[0]);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CycleLoopsToMiddleBruteForceTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4, 5 });
+            nodes[4].Next = nodes[2];
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(nodes[0]);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TwoNodeCycleAtEndBruteForceTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4, 5 });
+            nodes[4].Next = nodes[3];
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(nodes[0]);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EmptyListBruteForceTest()
+        {
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(null);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void OneElementLinkedListNoCycleBruteForceTest()
+        {
+            var node = new LinkedListNode(1);
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(node);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void OneElementLinkedListCycleBruteForceTest()
+        {
+            var node = new LinkedListNode(1);
+            node.Next = node;
+            var result = DetectCycle.ContainsCycleBruteForceUsingSet(node);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void LinkedListWithNoCycleTwoRunnersTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(nodes[0]);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CycleLoopsToBeginningTwoRunnersTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            nodes[3].Next = nodes[0];
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(nodes[0]);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CycleLoopsToMiddleTwoRunnersTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4, 5 });
+            nodes[4].Next = nodes[2];
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(nodes[0]);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TwoNodeCycleAtEndTwoRunnersTest()
+        {
+            var nodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4, 5 });
+            nodes[4].Next = nodes[3];
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(nodes[0]);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EmptyListTwoRunnersTest()
+        {
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(null);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void OneElementLinkedListNoCycleTwoRunnersTest()
+        {
+            var node = new LinkedListNode(1);
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(node);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void OneElementLinkedListCycleTwoRunnersTest()
+        {
+            var node = new LinkedListNode(1);
+            node.Next = node;
+            var result = DetectCycle.ContainsCycleEfficientUsingFastSlowRunners(node);
+            Assert.True(result);
+        }
+
+        private static LinkedListNode[] ValuesToLinkedListNodes(int[] values)
+        {
+            var nodes = new LinkedListNode[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                nodes[i] = new LinkedListNode(values[i]);
+                if (i > 0)
+                {
+                    nodes[i - 1].Next = nodes[i];
+                }
+            }
+            return nodes;
+        }
+
+    }
+}
---
+++ InterviewCake/linked-lists/singly-linked/DetectCycle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCake.linked_lists.singly_linked
+{
+    public class DetectCycle
+    {
+        // O(n) time (which is OK ) & space (can be improved)
+        public static bool ContainsCycleBruteForceUsingSet(LinkedListNode firstNode)
+        {
+            // Check if the linked list contains a cycle
+            HashSet<LinkedListNode> set = new HashSet<LinkedListNode>();
+
+            LinkedListNode node = firstNode;
+
+            while (node != null)
+            {
+                if (set.Contains(node)) return true;
+
+                set.Add(node);
+
+                node = node.Next;
+            }
+
+            return false;
+        }
+
+        public static bool ContainsCycleEfficientUsingFastSlowRunners(LinkedListNode firstNode)
+        {
+            LinkedListNode slowRunner = firstNode;
+            LinkedListNode fastRunner = firstNode;
+
+            while (fastRunner != null && fastRunner.Next != null)
+            {
+                slowRunner = slowRunner.Next;
+                fastRunner = fastRunner.Next.Next;
+
+                if (fastRunner == slowRunner) return true;
+            }
+
+            return false;
+        }
+    }
+}
