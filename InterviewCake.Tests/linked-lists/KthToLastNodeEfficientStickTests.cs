dc30e3f387de559bb29e4d05fa48a2870df3b79e
Arun Ramachandriya <arun.ramachandriya@investec.co.uk>
2019-04-16 15:04:13 +0100

    Singly LL: Kth from last node - both naive & efficient implementation

---
+++ InterviewCake.Tests/linked-lists/KthToLastNodeEfficientStickTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterviewCake.linked_lists;
+using InterviewCake.linked_lists.singly_linked;
+using Xunit;
+
+namespace InterviewCake.Tests.linked_lists
+{
+    public class KthToLastNodeEfficientStickTests
+    {
+        [Fact]
+        public void FirstToLastNodeTest()
+        {
+            var listNodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var k = 1;
+            var actual = KthToLastNode.GetNodeByEfficientStickMethod(k, listNodes[0]);
+            var expected = listNodes[listNodes.Length - k];
+            Assert.Same(expected, actual);
+        }
+
+        [Fact]
+        public void SecondToLastNodeTest()
+        {
+            var listNodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var k = 2;
+            var actual = KthToLastNode.GetNodeByEfficientStickMethod(k, listNodes[0]);
+            var expected = listNodes[listNodes.Length - k];
+            Assert.Same(expected, actual);
+        }
+
+        [Fact]
+        public void FirstNodeTest()
+        {
+            var listNodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var k = 4;
+            var actual = KthToLastNode.GetNodeByEfficientStickMethod(k, listNodes[0]);
+            var expected = listNodes[listNodes.Length - k];
+            Assert.Same(expected, actual);
+        }
+
+        [Fact]
+        public void KIsGreaterThanLinkedListLengthTest()
+        {
+            var listNodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var k = 5;
+            Assert.Throws<ArgumentException>(() => KthToLastNode.GetNodeByEfficientStickMethod(k, listNodes[0]));
+        }
+
+        [Fact]
+        public void KIsZeroTest()
+        {
+            var listNodes = ValuesToLinkedListNodes(new int[] { 1, 2, 3, 4 });
+            var k = 0;
+            Assert.Throws<ArgumentException>(() => KthToLastNode.GetNodeByEfficientStickMethod(k, listNodes[0]));
+        }
+
+        private static LinkedListNode[] ValuesToLinkedListNodes(int[] values)
+        {
+            var nodes = new LinkedListNode[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                nodes[i] = new LinkedListNode(values[i]);
+                if (i > 0)
+                {
+                    nodes[i - 1].Next = nodes[i];
+                }
+            }
+            return nodes;
+        }
+    }
+}
---
+++ InterviewCake/linked-lists/singly-linked/KthToLastNode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCake.linked_lists.singly_linked
+{
+    public class KthToLastNode
+    {
+        public static LinkedListNode GetNodeByNaiveMethod(int k, LinkedListNode head)
+        {
+            // Return the kth to last node in the linked list
+
+            if (k == 0) throw new ArgumentException();
+
+            // Get length of the LL
+            int length = 0;
+
+            LinkedListNode currNode = head;
+            while (currNode != null)
+            {
+                length++;
+                currNode = currNode.Next;
+            }
+
+            if (k > length) throw new ArgumentException();
+
+            int countFromHead = length - k;
+            int counter = 0; // should start from 0 not 1 eg if length is 4 and k is 4 then we need the first item at counter == (length - k) == 0
+
+            currNode = head;
+
+            while (currNode != null)
+            {
+                if (counter == countFromHead) break;
+
+                currNode = currNode.Next;
+                counter++;
+            }
+
+            return currNode;
+        }
+
+        public static LinkedListNode GetNodeByEfficientStickMethod(int k, LinkedListNode head)
+        {
+            if (k == 0) throw new ArgumentException();
+
+            LinkedListNode trailingPointer = head;
+            LinkedListNode forwardPointer = head;
+
+            for (int counter = 2; counter <= k; counter++)
+            {
+                forwardPointer = forwardPointer.Next;
+                if (forwardPointer == null) throw new ArgumentException();
+            }
+
+            while (forwardPointer.Next != null)
+            {
+                forwardPointer = forwardPointer.Next;
+                trailingPointer = trailingPointer.Next;
+            }
+
+            return trailingPointer;
+        }
+    }
+}
